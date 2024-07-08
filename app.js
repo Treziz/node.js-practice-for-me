aef1b47bd9fb7ef8aacdbd23a7f4cff697025527
Nathaphon Paripunna <bosqwe2542@hotmail.com>
2024-07-08 16:21:05 +0700

    add files to project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan =require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req, res) => {
+    res.send('hello borntoDev');
+});
+
+app.listen(port, async () => {
+    const chalk = await import('chalk');
+    debug(`Listening on port ${chalk.default.green(port)}`);
+});
